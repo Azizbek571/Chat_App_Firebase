c622521f4f54e0038958892efe7d66a53edb2d92
Azizbek <azizbek.mukhtorov.m@mail.ru>
2024-08-10 01:10:10 +0500

    first commit

---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> setupFirebase()async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform);
+}
