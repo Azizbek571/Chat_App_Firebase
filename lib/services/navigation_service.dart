845dacf812632fac83aac892a4319bb866c341ba
Azizbek <azizbek.mukhtorov.m@mail.ru>
2024-08-13 11:24:45 +0500

    login

---
+++ lib/services/navigation_service.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_chat_app/pages/home_page.dart';
+import 'package:firebase_chat_app/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class NavigationService{
+  late GlobalKey<NavigatorState> _navigatorKey;
+  final Map<String , Widget Function(BuildContext)> _routes = {
+    "/login": (context) => LoginPage(),
+    "/home": (context) => HomePage(),
+  };
+GlobalKey <NavigatorState>? get navigatorKey{
+  return _navigatorKey;
+}
+  
+     Map<String , Widget Function(BuildContext)> get routes {
+      return _routes;
+     }
+
+
+  NavigationService(){
+    _navigatorKey = GlobalKey<NavigatorState>();
+  }
+
+  void pushNamed(String routeName){
+    _navigatorKey.currentState?.pushNamed(routeName);
+  }
+  void pushReplacementNamed(String routeName){
+    _navigatorKey.currentState?.pushReplacementNamed(routeName);
+  }
+
+  void goBack(){
+    _navigatorKey.currentState?.pop();
+  }
+}
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,17 @@
 import 'package:firebase_chat_app/firebase_options.dart';
+import 'package:firebase_chat_app/services/alert_services.dart';
+import 'package:firebase_chat_app/services/auth_service.dart';
+import 'package:firebase_chat_app/services/navigation_service.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
 
-Future<void> setupFirebase()async{
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform);
+Future<void> setupFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+
+Future<void> registerServices() async {
+  final GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<AuthService>(AuthService());
+  getIt.registerSingleton<NavigationService>(NavigationService());
+  getIt.registerSingleton<AlertService>(AlertService());
 }
